d9fb9507545522a76552a0c38ab873778081ae92
khdj <32709704+khdj@users.noreply.github.com>
2017-12-10 21:45:33 +0100

    Derniers changements pour ce soir

--- admin/js/classJS/preparation/InterfacePreparation.js
+++ admin/js/classJS/preparation/InterfacePreparation.js
@@ -1,4 +1,4 @@
-document.getElementById("commande").style.visibility = "hidden";
+document.getElementById("commande").style.visibility = "hidden"; //display : none;
 
 /*var test = document.getElementById("commande");
 alert(test);*/
@@ -16,10 +16,12 @@ function impressionBon(){
 }
 
 function afficheCommande(nb){
-  document.getElementById("commande").style.visibility = "visible";
+  document.getElementById("commande").style.visibility = "visible"; //display : initial
   var commande = "commande"+nb;
   document.getElementById(commande).style.transitionDuration = "0.4s";
   document.getElementById(commande).style.backgroundColor = "#138fef";
+  document.getElementById(commande).style.color = "white";
+  document.getElementById(commande).style.fontWeight = "bold";
   //alert("Commande "+nb);
 }
 //document.getElementById("commandeUn");
