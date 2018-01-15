9abde762ef5f0cd651e4af74ac1db0993504fab3
khdj <32709704+khdj@users.noreply.github.com>
2018-01-15 10:01:11 +0100

    Ajout des boutons expedier et ilprimer facture bon de livraison

--- admin4815/js/classJS/preparation/InterfacePreparation.js
+++ admin4815/js/classJS/preparation/InterfacePreparation.js
@@ -1,13 +1,3 @@
-0//alert("interfprep");
-//document.getElementById("commande").style.visibility = "hidden"; //display : none;
-
-/*unction afficheCommandes(){
-  //console.log("Dans la fonction affiche commande");
-  alert(Preparation.RechercheExpeditions());
-  //alert(document.getElementById("panels-8").innerHTML);
-}*/
-
-//var test = ["Naruto","Sasuke","Kakashi","Obito"];
 
 var InterfacePreparation = {
 
@@ -18,21 +8,17 @@ var InterfacePreparation = {
       var row = "<tr class = 'ligCommande'><td>"+data[com].refCommande+"</td>";
       row += "<td>"+data[com].livraison.nom+" "+data[com].livraison.prenom+"</td>";
       row += "<td>"+data[com].livraison.adresse+"</td>";
-      //document.getElementById("preparationCommandes").innerHTML += "<tr><td>"+data[com].refCommande+"</td>";
-      //document.getElementById("preparationCommandes").innerHTML += "<td>"+data[com].livraison.nom+" "+data[com].livraison.prenom+"</td>";
-      //document.getElementById("preparationCommandes").innerHTML += "<td>"+data[com].livraison.adresse+"</td>";
 
       var prixTotal = 0;
-      var refs = [];  //maps et qté sont des variables pour la suite (le cadre avec l'image)
+      var refs = [];  //refs et qté sont des variables pour la suite (le cadre avec l'image)
       var qte = [];   //ca évite de parcourir de nouveau pour récupérer les infos
+
       for(prod in data[com].commande){
         prixTotal +=parseFloat(data[com].commande[prod].prixTTC);
         refs.push(data[com].commande[prod].ref);
         qte.push(data[com].commande[prod].quantite);
-        //console.log(data[com].ID +"  "+data[com].commande[prod].ref+"  "+prixTotal);
       }
       row += "<td>"+prixTotal.toFixed(2)+"</td></tr>";
-      //document.getElementById("preparationCommandes").innerHTML += "<td>"+prixTotal+"</td></tr>";
       document.getElementById("preparationCommandes").innerHTML += row;
 
       var order = "<tr><td id='cadreCommande' colspan=4><span id = 'numCommande'>"+data[com].ID+"</span></br>";
@@ -42,97 +28,42 @@ var InterfacePreparation = {
         order += "<span> Réference : "+refs[i]+"</span>";
         order += "<span> (X "+qte[i]+")</span></div>";
       }
-      order += "</td></tr>";
+      order += "<span><button class='boutonsCommande' id='boutonExpedition'>Expédier</button>";
+      order += "<button class='boutonsCommande' id='boutonFacture'>Imprimer facture</button>"
+      order += "<button class='boutonsCommande' id='boutonBon'>Imprimer bon de livraison</button>"
+      order += "</span></td></tr>";
 
       document.getElementById("preparationCommandes").innerHTML += order;
 
     }
-
-    //var ligs = document.querySelector("#preparationCommandes > td");
-    //ligs.setAttribute("class","ligCommande");
-    //alert(document.getElementsByClassName("ligCommande"));
-
   },
 
   afficheCommande : function(data) {
 
-      var row = "<tr class = 'ligCommande'><td>"+data[0].refCommande+"</td>";
-      row += "<td>"+data[0].livraison.nom+" "+data[0].livraison.prenom+"</td>";
-      row += "<td>"+data[0].livraison.adresse+"</td>";
+      var row = "<tr class = 'ligCommande'><td>"+data.refCommande+"</td>";
+      row += "<td>"+data.livraison.nom+" "+data.livraison.prenom+"</td>";
+      row += "<td>"+data.livraison.adresse+"</td>";
 
       var prixTotal = 0;
-      for(prod in data[0].commande){
-        prixTotal +=parseFloat(data[0].commande[prod].prixTTC);
+      for(prod in data.commande){
+        prixTotal +=parseFloat(data.commande[prod].prixTTC);
       }
       row += "<td>"+prixTotal.toFixed(2)+"</td></tr>";
 
       document.getElementById("preparationCommandes").innerHTML += row;
 
-
-    /*var ligs = document.querySelector("#preparationCommandes > table > td");
-    ligs.setAttribute("class","ligCommande");
-    alert(document.getElementsByClassName("ligCommande"));*/
-
-},
+  },
 
   alertMessage : function(msg) {
-    //alert(msg);
+
     document.getElementById("panels-8").innerHTML += "<p>"+msg+"</p>";
     var message = document.querySelector("#panels-8 > p");
     message.setAttribute("id","alert");
+
+  },
+
+  effaceCommande : function(data){
+
   }
 
 };
-
-
-
-/*var test = document.getElementById("commande");
-alert(test);*/
-
-/*JNTP.execute(["getExpedition", {"filter" : {"ID": 378}}], function(j){
-	displayListPreparation();
-})*/
-
-/*var Preparation = {
-value : [],
-RechercheAllExpeditions : function(){
-JNTP.execute(["getExpedition", {"filter" :{}}], function(j){
-if(j.body.length){
-this.value = j.body;
-
-}
-else{
-alertMsg("Aucune expedition prévu");
-}
-}.bind(this));
-}*/
-
-/*function expedition(){
-	alert('On expedie le colis');
-}
-
-expedition();
-
-function impressionFacture(){
-	alert('On imprime la facture');
-	print();
-}
-
-function impressionBon(){
-	alert('On imprime le bon de livraison');
-}
-
-function afficheCommande(nb){
-  document.getElementById("commande").style.visibility = "visible"; //display : initial
-  var commande = "commande"+nb;
-  document.getElementById(commande).style.transitionDuration = "0.4s";
-  document.getElementById(commande).style.backgroundColor = "#138fef";
-  document.getElementById(commande).style.color = "white";
-  document.getElementById(commande).style.fontWeight = "bold";
-  //alert("Commande "+nb);
-}
-
-/*$("#expedition").click(function(){
-	expedition();
-})*/
-//document.getElementById("commandeUn");
--- admin4815/js/classJS/preparation/Preparation.js
+++ admin4815/js/classJS/preparation/Preparation.js
@@ -23,13 +23,25 @@ var Preparation = {
 			if(j.body.length != 0){
 				//alert("ici");
 				this.donnees = j.body;
-				InterfacePreparation.afficheCommande(this.donnees);
+				InterfacePreparation.afficheCommande(this.donnees[0]);
 			} else InterfacePreparation.alertMessage("Aucune expedition prévue pour la commande d'ID : "+id);
 			//document.getElementById("preparationCommandes").innerHTML += "<p>Aucune expedition prévue pour la commande d'ID : "+id+"</p>";
 		})
 
-	
+	},
+
+	ExpedierCommande : function(IDdeLaCommande) {
+		JNTP.execute(["setExpedition", {"expedier" : IDdeLaCommande}],function(j){
+			if(j.body.length != 0){
+				this.donnees = j.body;
+				j.expedition = true; //je crois, faut que je demande à Athmane
+				InterfacePreparation.effaceCommande(this.donnees);
+			}
+			else InterfacePreparation.alertMessage("La commande d'ID : "+ IDdeLaCommande +" ne peut être expédiée");
+
+		})
 	}
+
 
 };
 
