4d734bd0518e359c6a2f4d18deabf9bf66701574
khdj <32709704+khdj@users.noreply.github.com>
2018-01-15 17:38:10 +0100

    recup 2

--- admin4815/js/classJS/preparation/Preparation.js
+++ admin4815/js/classJS/preparation/Preparation.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-//alert("prep.js");
-=======
->>>>>>> 83c9f12a637b4a1f9f96f1a4fdeb73ab84f8685e
 
 var Preparation = {
 	donnees : [],
@@ -33,26 +29,16 @@ var Preparation = {
 
 	},
 
-<<<<<<< HEAD
-	ExpedierCommande : function(IDdeLaCommande) {
-		JNTP.execute(["setExpedition", {"expedier" : IDdeLaCommande}],function(j){
-			if(j.body.length != 0){
-				this.donnees = j.body;
-				j.expedition = true; //je crois, faut que je demande à Athmane
-=======
 	ExpedierCommande : function(IDdeLaCommande) {	//comme la requête ne fonctionne pas, c'est encore expérimental
 		JNTP.execute(["setExpedition", {"expedier" : IDdeLaCommande}],function(j){
 			if(j.body.length != 0){
 				this.donnees = j.body;
 				j.expedition = true;
->>>>>>> 83c9f12a637b4a1f9f96f1a4fdeb73ab84f8685e
 				InterfacePreparation.effaceCommande(this.donnees);
 			}
 			else InterfacePreparation.alertMessage("La commande d'ID : "+ IDdeLaCommande +" ne peut être expédiée");
 
 		})
-<<<<<<< HEAD
-=======
 	},
 
 	ImprimerFacture : function(IDdeLaCommande) {
@@ -71,14 +57,9 @@ var Preparation = {
 			}
 			else InterfacePreparation.alertMessage("Impossible d'imprimer le bon de livraison pour la commande d'ID : "+ IDdeLaCommande);
 		})
->>>>>>> 83c9f12a637b4a1f9f96f1a4fdeb73ab84f8685e
 	}
 
 
 };
 
 $("#preparationPanel").click(Preparation.RechercheAllExpeditions());
-<<<<<<< HEAD
-//$("#preparationPanel").click(Preparation.RechercheExpedition(28));
-=======
->>>>>>> 83c9f12a637b4a1f9f96f1a4fdeb73ab84f8685e
